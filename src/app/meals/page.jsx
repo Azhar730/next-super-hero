fda1e5a2402f1f0b325f8b4dc3cbebc3d3ffa752
Azhar730 <azharmahmud730@gmail.com>
2024-09-14 12:56:17 +0600

    second commit done

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -13,7 +13,7 @@ const Meals = () => {
 
     return (
         <div className='p-6'>
-            <h1 className={`my-2 text-2xl font-semibold text-gray-500`}>Choose your Meals</h1>
+            <h1 className={`my-2 text-2xl font-semibold text-gray-500`}>Choose your Meals From Here</h1>
             <MealsComp />
         </div>
     );
